48d664acd9c6a5b1c9d373b2037288d4fc7ae16c
Enda Phelan <ephelan@redhat.com>
2019-04-21 20:19:06 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import { Alert } from "./Alert";
 
 const App = () => {
@@ -7,29 +7,33 @@ const App = () => {
   const [duration, setDuration] = useState(1000);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="input-wrapper">
-          <input
-            value={duration}
-            type="text"
-            onChange={e => {
-              const val = e.target.value || 0;
-              setDuration(parseInt(val));
-            }}
-          />
+    <div className="container">
+      <div className="align-middle p-3">
+        <Form inline>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <Input
+              value={duration}
+              type="text"
+              onChange={e => {
+                const val = e.target.value || 0;
+                setDuration(parseInt(val));
+              }}
+            />
+          </FormGroup>
+          <Button color="primary" onClick={() => setAlertVisibility(true)}>
+            Show Alert
+          </Button>
+        </Form>
+        <div className="p-3">
+          <Alert
+            visible={visible}
+            duration={duration}
+            onDurationEnd={setAlertVisibility}
+          >
+            <div>Alert timeout is set to [{duration}]</div>
+          </Alert>
         </div>
-        <div className="button-wrapper">
-          <button onClick={() => setAlertVisibility(true)}>Show Alert</button>
-        </div>
-        <Alert
-          visible={visible}
-          duration={duration}
-          onDurationEnd={setAlertVisibility}
-        >
-          <div>Alert timeout is set to [{duration}]</div>
-        </Alert>
-      </header>
+      </div>
     </div>
   );
 };
